38ca2e234367d80956a08eba04a1f3ad2e6bce06
Bob Gendler <boberito@mac.com>
2018-09-26 23:43:27 -0400

    Update and rename AppleCacheServerStatsCSV.sh to AppleCacheServerStatsCSV.py

---
+++ AppleCacheServerStatsCSV.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+##################################################################
+#Makes a CSV of the amount served and downloaded from apple and date
+#Make a launchdaemon for it to run right after midnight sometime.
+##################################################################
+
+import subprocess
+import StringIO
+
+outputfile = open("/Users/Shared/output.csv", "a")
+
+command = "log show  --predicate \'subsystem == \"com.apple.AssetCache\"\' --debug --info --start \"$(date -v -1d \"+%Y-%m-%d\")\" --end \"$(date \"+%Y-%m-%d\")\" | grep \"Served all\""
+
+cacheoutput = subprocess.check_output(command, shell=True)
+rawLog = StringIO.StringIO(cacheoutput)
+
+total = 0
+localcache = 0
+appledownload = 0
+
+for x in rawLog:
+    linesplit = str.split(x)
+    if linesplit[16] == "bytes;":
+        if linesplit[15] != "0":
+            total = total + (float(linesplit[15])/1024/1024/1024)
+    if linesplit[16] == "KB;":
+        total = total + (float(linesplit[15])/1024/1024)
+    if linesplit[16] == "MB;":
+        total = total + (float(linesplit[15])/1024)
+    if linesplit[16] == "GB;":
+        total = total + float(linesplit[15])
+    
+    if linesplit[18] == "bytes":
+        if linesplit[17] != "0":
+            localcache = localcache + (float(linesplit[17])/1024/1024/1024)
+    if linesplit[18] == "KB":
+        localcache = localcache + (float(linesplit[17])/1024/1024)
+    if linesplit[18] == "MB":
+        localcache = localcache + (float(linesplit[17])/1024)
+    if linesplit[18] == "GB":
+        localcache = localcache + float(linesplit[17])
+    
+    if linesplit[22] == "bytes":
+        if linesplit[21] != "0":
+            appledownload = appledownload + (float(linesplit[21])/1024/1024/1024)
+    if linesplit[22] == "KB":
+        appledownload = appledownload + (float(linesplit[21])/1024/1024)
+    if linesplit[22] == "MB":
+        appledownload = appledownload + (float(linesplit[21])/1024)
+    if linesplit[22] == "GB":
+        appledownload = appledownload + float(linesplit[21])
+	
+output = "%.2f,%.2f,%.2f\n" % (round(total,2), round(localcache,2), round(appledownload,2))
+outputfile.write(output)
+outputfile.close()
